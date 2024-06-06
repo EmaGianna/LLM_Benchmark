89bcaa09164c38b8edb19e78a8d464691b6c0197
Emanuel Giannattasio <emanuel.giannattasio@gmail.com>
2024-06-05 22:19:05 -0300

    Update_varios

--- nbk/lib_llm_benchmark/utils.py
+++ nbk/lib_llm_benchmark/utils.py
@@ -14,3 +14,63 @@ def list_to_dict(in_list):
         dict_[i] = valor
 
     return dict_
+
+
+def  return_numeric_df(df):
+    """
+    The function `return_numeric_df` selects numeric columns from a DataFrame and renames the columns
+    with specific names.
+    
+    :param df: It looks like the function `return_numeric_df` takes a DataFrame as input and returns a
+    new DataFrame containing only the columns with numeric data types. Additionally, it renames the
+    columns of the new DataFrame with specific names
+    :return: A DataFrame containing only the columns with numeric data types, with renamed columns.
+    """
+    df = df.select_dtypes(include=['number'])
+    df.columns = ['id_promtp','tiempo de lectura','cantidad de oraciones','cantidad de caracteres','cantidad de letras'
+                ,'cantidad de silabas','largo promedio de oraciones','promedio de letras por palabra','cantidad de stopwords'
+                ,'cantidad de palabras','densidad lexica','riqueza lexica']
+    return df
+
+def create_scatter_graph(df_llama, df_gemma, column):
+    """
+    The function `create_scatter_graph` generates a scatter graph comparing data from two DataFrames for
+    a specified column.
+    
+    :param df_llama: The `df_llama` parameter is a DataFrame containing data related to the Llama model.
+    It likely includes columns such as 'id_prompt' and the specified `column` for comparison in the
+    scatter graph
+    :param df_gemma: The function `create_scatter_graph` you provided creates a scatter graph comparing
+    a specific column between two dataframes, `df_llama` and `df_gemma`. The `column` parameter
+    specifies which column to compare
+    :param column: The `column` parameter in the `create_scatter_graph` function is used to specify
+    which column from the dataframes `df_llama` and `df_gemma` you want to compare in the scatter graph.
+    This column will be plotted on the y-axis, while the 'ID Prompt'
+    """
+
+    import plotly.graph_objects as go
+    # Crear la figura
+    fig = go.Figure()
+
+    # Añadir la primera gráfica de líneas (Llama)
+    fig.add_trace(go.Scatter(x=df_llama['id_promtp'], y=df_llama[column],
+                             mode='lines+markers', name='Llama'))
+    ## Añadir la segunda gráfica de líneas (Gemma)
+    fig.add_trace(go.Scatter(x=df_gemma['id_promtp'], y=df_gemma[column],
+                             mode='lines+markers', name='Gemma'))
+
+    # Configurar el título y los ejes
+    fig.update_layout(
+        title=f'Comparación de {column}',
+        xaxis_title='ID Prompt',
+        yaxis_title=column,
+        legend_title='Modelo LLM',
+        xaxis=dict(
+            tickmode='linear',  # Usar modo de ticks lineal
+            tick0=1,            # Primer tick en el valor 1
+            dtick=1             # Distancia entre ticks igual a 1
+        )
+    )
+
+    # Mostrar el gráfico
+    fig.show()
