5df1720e8eb06474ce4d6773123dfc2a02062534
Emanuel Giannattasio <emanuel.giannattasio@gmail.com>
2024-07-24 20:08:54 -0300

    update_source_20240724
    se actualiza librerias y se agrega directorio con htmls interactivos

--- nbk/lib_llm_benchmark/utils.py
+++ nbk/lib_llm_benchmark/utils.py
@@ -32,20 +32,23 @@ def  return_numeric_df(df):
                 ,'cantidad de palabras','densidad lexica','riqueza lexica']
     return df
 
-def create_scatter_graph(df_llama, df_gemma, column):
+def create_scatter_graph(df_llama, df_gemma, column, path_save_html):
     """
-    The function `create_scatter_graph` generates a scatter graph comparing data from two DataFrames for
-    a specified column.
+    The function `create_scatter_graph` generates a scatter graph comparing data from two DataFrames and
+    saves it as an HTML file.
     
     :param df_llama: The `df_llama` parameter is a DataFrame containing data related to the Llama model.
-    It likely includes columns such as 'id_prompt' and the specified `column` for comparison in the
-    scatter graph
-    :param df_gemma: The function `create_scatter_graph` you provided creates a scatter graph comparing
-    a specific column between two dataframes, `df_llama` and `df_gemma`. The `column` parameter
-    specifies which column to compare
+    It is used to plot a scatter graph comparing a specific column between the Llama and Gemma models
+    :param df_gemma: The `df_gemma` parameter is a DataFrame containing data related to the model Gemma.
+    It is used in the function `create_scatter_graph` to plot a scatter graph comparing a specific
+    column between two models, Llama and Gemma
     :param column: The `column` parameter in the `create_scatter_graph` function is used to specify
     which column from the dataframes `df_llama` and `df_gemma` you want to compare in the scatter graph.
-    This column will be plotted on the y-axis, while the 'ID Prompt'
+    It represents the specific column data that you want to visualize in the graph
+    :param path_save_html: The `path_save_html` parameter in the `create_scatter_graph` function is the
+    file path where you want to save the HTML file of the scatter graph that will be generated. You can
+    specify the directory where you want to save the HTML file along with the desired filename. For
+    example, you
     """
 
     import plotly.graph_objects as go
@@ -74,3 +77,5 @@ def create_scatter_graph(df_llama, df_gemma, column):
 
     # Mostrar el gráfico
     fig.show()
+    # Guardar grafico en html
+    fig.write_html(f"{path_save_html}{column.replace(' ','_')}.html")
